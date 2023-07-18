67b642cb14204d50477ec4e04f4227dc8f8c8706
Nicholas Cash <nicholasc@jamcity.com>
2023-07-18 16:34:50 -0500

    Add new transition-jira-status action

---
+++ transition-jira-status/index.js
@@ -0,0 +1,78 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fetch = require('node-fetch');
+
+
+async function run()
+{
+    try
+    {
+        const context = github.context;
+        const jiraProjectPrefix = core.getInput('jira_project_prefix', { required: true }).toUpperCase();
+        const jiraStatusId = core.getInput('jira_status_id', { required: true });
+        const jiraStatusTransitionId = core.getInput('jira_status_transition_id', { required: true });
+
+        const jiraNumber = context.payload.pull_request.title.match(new RegExp(`^${jiraProjectPrefix}-[0-9]+`, "g"));
+
+        if (jiraNumber)
+        {
+            const jiraTicketNumber = jiraNumber[0];
+
+            var request = {
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Basic ${Buffer.from(`${core.getInput('jira_user')}:${core.getInput('jira_password')}`).toString('base64')}`,
+                }
+            };
+
+            var jiraResponse = await fetch(`https://socialgamingnetwork.jira.com/rest/api/latest/issue/${jiraTicketNumber}?fields=status`, request);
+            var jiraJSON = await jiraResponse.json();
+
+            core.debug(jiraJSON);
+
+            if (jiraResponse.ok && (jiraResponse.fields.status.id == jiraStatusId))
+            {
+                var request = {
+                    method: 'POST',
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Basic ${Buffer.from(`${core.getInput('jira_user')}:${core.getInput('jira_password')}`).toString('base64')}`,
+                    },
+                    body: JSON.stringify({
+                        transition: {
+                            id: jiraStatusTransitionId
+                        }
+                    })
+                };
+
+                core.debug(request);
+
+                var jiraPOSTResponse = await fetch(`https://socialgamingnetwork.jira.com/rest/api/latest/issue/${jiraTicketNumber}/transitions`, request);
+                var jiraPostJSON = await jiraResponse.json();
+
+                core.debug(jiraPostJSON);
+
+                if (!jiraPOSTResponse.ok)
+                {
+                    core.setFailed('There was an issue using the JIRA API to transition the ticket status');
+                }
+            }
+            else
+            {
+                core.setFailed('There was an issue fetching ticket status with the JIRA API');
+            }
+        }
+        else
+        {
+          core.info('No JIRA ticket was found in the pull request title :(');
+        }
+    }
+    catch (error)
+    {
+        core.setFailed(error.message);
+    }
+}
+
+run();
